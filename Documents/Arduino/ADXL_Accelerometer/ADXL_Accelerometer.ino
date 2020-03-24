e46a0fa06f38abe8fc7683bb06ee71daf6108d00
Wagner Dantas <wagnerdantas@Wagners-MacBook-Pro-496.local>
2020-03-24 12:13:48 +0000

    Acceleroeter and Led blink

---
+++ Documents/Arduino/ADXL_Accelerometer/ADXL_Accelerometer.ino
@@ -0,0 +1,110 @@
+  /*
+    Arduino and ADXL345 Accelerometer Tutorial
+     by Dejan, https://howtomechatronics.com
+*/
+
+#include <Wire.h>  // Wire library - used for I2C communication
+
+
+int ADXL345 = 0x53; // The ADXL345 sensor I2C address
+
+float X_out, Y_out, Z_out;  // Outputs
+
+void setup() {
+  Serial.begin(9600); // Initiate serial communication for printing the results on the Serial monitor
+  Wire.begin(); // Initiate the Wire library
+  // Set ADXL345 in measuring mode
+  Wire.beginTransmission(ADXL345); // Start communicating with the device 
+  Wire.write(0x2D); // Access/ talk to POWER_CTL Register - 0x2D
+  // Enable measurement
+  Wire.write(8); // (8dec -> 0000 1000 binary) Bit D3 High for measuring enable 
+  Wire.endTransmission();
+  //delay(100);
+
+
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+
+}
+
+void loop() {
+  // === Read acceleromter data === //
+  Wire.beginTransmission(ADXL345);
+  Wire.write(0x32); // Start with register 0x32 (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(ADXL345, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
+  X_out = ( Wire.read()| Wire.read() << 8); // X-axis value
+  X_out = X_out/256; //For a range of +-2g, we need to divide the raw values by 256, according to the datasheet
+  Y_out = ( Wire.read()| Wire.read() << 8); // Y-axis value
+  Y_out = Y_out/256;
+  Z_out = ( Wire.read()| Wire.read() << 8); // Z-axis value
+  Z_out = Z_out/256;
+
+  Serial.print("Xa= ");
+  Serial.print(X_out);
+  Serial.print("   Ya= ");
+  Serial.print(Y_out);
+  Serial.print("   Za= ");
+  Serial.println(Z_out);
+
+  digitalWrite(8,HIGH);
+
+
+
+  /*COMMENT*/
+
+  /*
+  while (X_out > 0.3) {
+
+    keepRightBlink();    
+  } 
+*/
+
+
+    while (X_out < -0.3){
+
+      Serial.print("Entrou no IF 1 1 1 1 1 1 1 1 1 1 1 1 1");
+
+      keepLeftBlink(); // inicia a funcao de pisca
+
+      X_out++ ; // retornar aos valores
+
+      if (Y_out < -0.3){ // verifica se o eixo Y_out e maior < -0.3
+            Serial.print("Entrou no W H I L E");
+            Serial.print("==========================================");
+            X_out++ ;
+           }
+      
+      
+
+      Serial.print("Entrou no IF 22 2 2 2 2 2 2 2 2 2 2");
+      
+       //X_out++  ;
+
+       
+          
+      
+    }
+
+    
+
+
+}
+
+int keepRightBlink(){
+      digitalWrite(7, HIGH);
+      delay(100);
+      digitalWrite(7, LOW);
+      delay(100);
+
+}
+
+int keepLeftBlink(){
+
+      digitalWrite(9, HIGH);
+      delay(100);
+      digitalWrite(9, LOW);
+      delay(100);
+
+}
