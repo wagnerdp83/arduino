411b2b22b1b93c83c63e68f3ddb338219ef2547d
Wagner Dantas <wagnerdantas@Wagners-MacBook-Pro-496.local>
2020-03-25 19:42:25 +0000

    Right arrow is working as defined

--- Documents/Arduino/ADXL_Accelerometer/ADXL_Accelerometer.ino
+++ Documents/Arduino/ADXL_Accelerometer/ADXL_Accelerometer.ino
@@ -10,26 +10,13 @@ int ADXL345 = 0x53; // The ADXL345 sensor I2C address
 
 float X_out, Y_out, Z_out;  // Outputs
 
-void setup() {
-  Serial.begin(9600); // Initiate serial communication for printing the results on the Serial monitor
-  Wire.begin(); // Initiate the Wire library
-  // Set ADXL345 in measuring mode
-  Wire.beginTransmission(ADXL345); // Start communicating with the device 
-  Wire.write(0x2D); // Access/ talk to POWER_CTL Register - 0x2D
-  // Enable measurement
-  Wire.write(8); // (8dec -> 0000 1000 binary) Bit D3 High for measuring enable 
-  Wire.endTransmission();
-  //delay(100);
+float X_trigger = -0.4;
 
+float Y_trigger = 0.4;
 
-  pinMode(7,OUTPUT);
-  pinMode(8,OUTPUT);
-  pinMode(9,OUTPUT);
+int stopBlink;
 
-}
-
-void loop() {
-  // === Read acceleromter data === //
+int startRead(){
   Wire.beginTransmission(ADXL345);
   Wire.write(0x32); // Start with register 0x32 (ACCEL_XOUT_H)
   Wire.endTransmission(false);
@@ -40,7 +27,35 @@ void loop() {
   Y_out = Y_out/256;
   Z_out = ( Wire.read()| Wire.read() << 8); // Z-axis value
   Z_out = Z_out/256;
+  
+ }
+ 
 
+
+void setup() {
+  Serial.begin(9600); // Initiate serial communication for printing the results on the Serial monitor
+  Wire.begin(); // Initiate the Wire library
+  
+  // Set ADXL345 in measuring mode
+  Wire.beginTransmission(ADXL345); // Start communicating with the device 
+  Wire.write(0x2D); // Access/ talk to POWER_CTL Register - 0x2D
+  // Enable measurement
+  Wire.write(8); // (8dec -> 0000 1000 binary) Bit D3 High for measuring enable 
+  Wire.endTransmission();
+  //delay(100);
+
+  // Pin LEDs
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  
+  digitalWrite(8,HIGH); // Break light should always be ON and blinking
+
+  //Read acceleromter data
+
+  startRead();
+
+  /*
   Serial.print("Xa= ");
   Serial.print(X_out);
   Serial.print("   Ya= ");
@@ -48,48 +63,44 @@ void loop() {
   Serial.print("   Za= ");
   Serial.println(Z_out);
 
-  digitalWrite(8,HIGH);
+  Serial.print("X Trigger ");
+  Serial.print(X_trigger);
+  Serial.print("  Y Trigger ");
+  Serial.print(Y_trigger);
+  */
+  
 
+}
 
-
-  /*COMMENT*/
-
-  /*
-  while (X_out > 0.3) {
-
-    keepRightBlink();    
-  } 
-*/
-
-
-    while (X_out < -0.3){
-
-      Serial.print("Entrou no IF 1 1 1 1 1 1 1 1 1 1 1 1 1");
-
-      keepLeftBlink(); // inicia a funcao de pisca
-
-      X_out++ ; // retornar aos valores
-
-      if (Y_out < -0.3){ // verifica se o eixo Y_out e maior < -0.3
-            Serial.print("Entrou no W H I L E");
-            Serial.print("==========================================");
-            X_out++ ;
-           }
-      
-      
-
-      Serial.print("Entrou no IF 22 2 2 2 2 2 2 2 2 2 2");
-      
-       //X_out++  ;
-
-       
-          
-      
-    }
-
+void loop() {
     
+    startRead();
+   
+ // Let the left arrow blinking
+ if (X_out < X_trigger){
 
+    X_trigger = X_out + 0.3;
+  
+    keepLeftBlink();
+    /*
+    Serial.print('\n');
+    Serial.print('\r');
+    Serial.print("============ X ==============");
+    Serial.print(X_out);
+    Serial.print('\n');
+    Serial.print('\r');
+    */
+   
+ } 
+ 
+ if(Y_out > Y_trigger){X_trigger = -0.4; } 
 
+    /*
+    Serial.print("============ X Trigger ==============");
+    Serial.print(X_trigger);
+    Serial.print('\n');
+    Serial.print('\r');
+    */
 }
 
 int keepRightBlink(){
@@ -97,11 +108,9 @@ int keepRightBlink(){
       delay(100);
       digitalWrite(7, LOW);
       delay(100);
-
 }
 
 int keepLeftBlink(){
-
       digitalWrite(9, HIGH);
       delay(100);
       digitalWrite(9, LOW);
